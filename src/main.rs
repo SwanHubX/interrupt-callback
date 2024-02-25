61bc45d37e4679913497e5e2a02e2d71648be79c
Ink <JQS4813@163.com>
2024-02-25 17:49:54 +0800

    Add loop
    先简单地实现了

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use std::env;
+use std::thread;
+use std::time::Duration;
+
+const BASE_URL: &str = "http://metadata.tencentyun.com/latest/meta-data/spot/termination-time";
+const TOKEN: &str = "ogUSAsQicRW1pOVfuq-rO";
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let webhook_url = &args[1]; // webhook url
+
+    loop {
+        // request metadata
+        let resp = reqwest::blocking::get(BASE_URL).unwrap();
+        let status = resp.status().as_u16();
+        if status == 200 {
+            // webhook notification
+            let client = reqwest::blocking::Client::new();
+            let resp = client.post(webhook_url)
+                .body("")
+                .header("Authorization", TOKEN)
+                .send().unwrap();
+            println!("status: {}", resp.status().as_u16());
+            break;
+        }
+        // delay 3s
+        thread::sleep(Duration::from_secs(3));
+    }
 }
