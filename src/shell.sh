56002061b4afae418afe9dc05f4b3c67e8ad3352
Zirui Cai <1575079435@qq.com>
2024-02-27 20:38:46 +0800

    init

---
+++ src/shell.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# 输出当前日期和时间
+echo "Current date and time:"
+date
+
+# 输出当前目录下的文件列表
+echo "Files in current directory:"
+ls
+
+# 执行一个简单的计算
+echo "Result of 3 + 5:"
+echo $((3 + 5))
+
+# 输出一条消息
+echo "Hello from the shell script!"
