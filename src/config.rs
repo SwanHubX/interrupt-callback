2c7e79374a88dca203eb892f476e2fb738f18234
Ink <55043165+swpfY@users.noreply.github.com>
2024-09-09 00:22:27 +0800

    add struct

--- src/config.rs
+++ src/config.rs
@@ -1,53 +1,99 @@
-use std::{fs, io};
-use std::path::Path;
+use crate::config::Provider::LocalHost;
+use reqwest::Url;
 use serde::Deserialize;
+use std::{error::Error, fs, path::Path};
 
 #[derive(Deserialize, Debug)]
 pub struct Config {
-    provider: Provider,
-    alert: Alert,
+    #[serde(default = "Provider::default")]
+    pub provider: Provider,
+    pub alert: Alert,
+    #[serde(default = "default_interval")]
+    pub interval: u16, // unit: second
+}
+
+// check every 5 seconds
+fn default_interval() -> u16 {
+    10
 }
 
 #[derive(Deserialize, Debug)]
-enum Provider {
+pub enum Provider {
     AliCloud,
     TencentCloud,
     LocalHost,
 }
 
-#[derive(Deserialize, Debug)]
-struct Alert {
-    feishu: Feishu,
+impl Default for Provider {
+    fn default() -> Self {
+        LocalHost
+    }
 }
 
 #[derive(Deserialize, Debug)]
-struct Feishu {
-    webhook: String,
+pub struct HeartBeat {
+    url: Url,
     secret: String,
 }
 
+#[derive(Deserialize, Debug)]
+pub struct Alert {
+    pub feishu: Feishu,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct Feishu {
+    #[serde(default)]
+    pub webhook: String,
+    pub secret: String,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct KeepAlive {
+    pub period: u16, // unit: second
+    pub client: Client,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct Client {
+    pub url: String,
+    pub key: String,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct Server {
+    pub key: String,
+    pub max: u16,
+}
+
 // load config file in toml format
-pub fn load_config(path: &Path) -> Result<String, io::Error> {
-    let con = fs::read_to_string(path)?;
-    println!("{}", con);
-    Ok(con)
+pub fn load_config(path: &Path) -> Result<String, Box<dyn Error>> {
+    let content = fs::read_to_string(path)?;
+    println!("{}", content);
+    let conf: Config = toml::from_str(&content)?;
+    println!("{:?}", conf);
+    println!("{}", conf.alert.feishu.webhook);
+    Ok(content)
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
-    use std::fs::File;
     use std::io::Write;
-    use tempfile::tempdir;
-
+    use tempfile::NamedTempFile;
 
     #[test]
-    fn test_load_config() {
-        let dir = tempdir().unwrap();
-        let path = dir.path().join("config.toml");
-        let mut f = File::create(&path).unwrap();
-        writeln!(f, "f001").unwrap();
-        println!("{:?}", dir.path());
-        load_config(&path).unwrap();
+    fn test_load_config() -> Result<(), Box<dyn Error>> {
+        let mut file = NamedTempFile::new()?;
+        writeln!(
+            file,
+            r#"
+            [alert.feishu]
+            webhook = "https://example.com"
+            secret = "ff"
+        "#
+        )?;
+        load_config(&file.path())?;
+        Ok(())
     }
 }
