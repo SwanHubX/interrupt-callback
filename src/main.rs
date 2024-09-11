2d9695f3417064052f1d8ccffee37b2561dd4451
Ink <JQS4813@163.com>
2024-09-11 20:00:21 +0800

    module: alert trait

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 mod config;
+mod alert;
+
 use log::{LevelFilter};
 use env_logger::Builder;
 
 fn main() {
-    Builder::new().filter_level(LevelFilter::Info).init()
+    Builder::new().filter_level(LevelFilter::Info).init();
 }
